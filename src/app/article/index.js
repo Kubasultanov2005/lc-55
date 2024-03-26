77f618a918f8c4cba20ada5734cef17cb23fbcd3
Kubasultanov2005 <kubasultanov2005@gmail.com>
2024-03-26 10:31:54 +0300

    first commit

---
+++ src/app/article/index.js
@@ -0,0 +1,62 @@
+import {memo, useCallback, useMemo} from 'react';
+import {useParams} from 'react-router-dom';
+import useStore from '../../hooks/use-store';
+import useTranslate from '../../hooks/use-translate';
+import useInit from '../../hooks/use-init';
+import PageLayout from '../../components/page-layout';
+import Head from '../../components/head';
+import Navigation from '../../containers/navigation';
+import Spinner from '../../components/spinner';
+import ArticleCard from '../../components/article-card';
+import LocaleSelect from '../../containers/locale-select';
+import TopHead from '../../containers/top-head';
+import {useDispatch, useSelector} from 'react-redux';
+import shallowequal from 'shallowequal';
+import articleActions from '../../store-redux/article/actions';
+import commentActions from '../../store-redux/comment/actions';
+import CommentSection from "../../containers/comment-section";
+
+function Article() {
+  const store = useStore();
+
+  const dispatch = useDispatch();
+  // Параметры из пути /articles/:id
+
+  const params = useParams();
+
+  useInit(() => {
+    //store.actions.article.load(params.id);
+    dispatch(articleActions.load(params.id));
+    dispatch(commentActions.load(params.id));
+  }, [params.id]);
+
+  const select = useSelector(state => ({
+    article: state.article.data,
+    waiting: state.article.waiting,
+    comments: state.comment.list,
+  }), shallowequal); // Нужно указать функцию для сравнения свойства объекта, так как хуком вернули объект
+
+  const {t} = useTranslate();
+
+  const callbacks = {
+    // Добавление в корзину
+    addToBasket: useCallback(_id => store.actions.basket.addToBasket(_id), [store]),
+  }
+
+
+  return (
+    <PageLayout>
+      <TopHead/>
+      <Head title={select.article.title}>
+        <LocaleSelect/>
+      </Head>
+      <Navigation/>
+      <Spinner active={select.waiting}>
+        <ArticleCard article={select.article} onAdd={callbacks.addToBasket} t={t}/>
+      </Spinner>
+      <CommentSection/>
+    </PageLayout>
+  );
+}
+
+export default memo(Article);
